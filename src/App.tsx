e63d49606425e71915079e1acfa570c4ab342134
Ubeom Kim <kimub1204@gmail.com>
2022-05-26 10:58:18 +0900

    refactor: 코드리뷰 피드백 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Hero from 'components/Hero/Hero';
 import Home from 'pages/Home/Home';
 import Rooms from 'pages/Rooms/Rooms';
-
-const CustomDiv = styled.div`
-  width: 50px;
-  height: 50px;
-  background-color: black;
-`;
-
-const CustomButton = styled(Button)`
-  width: 100px;
-  height: 100px;
-`;
 
 function App() {
   return (
