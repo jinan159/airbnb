6296abb2aa887d4a45b86aa3997c81d4542452ed
jinan159 <wlsdhks0423@naver.com>
2022-05-31 04:30:48 +0900

    feat: 숙소 가격 통계 API 가 더미 데이터를 반환하도록 변경

--- backend/src/main/java/codesquad/airbnb/accomodation/service/AccommodationStatisticsService.java
+++ backend/src/main/java/codesquad/airbnb/accomodation/service/AccommodationStatisticsService.java
@@ -1,50 +1,9 @@
 package codesquad.airbnb.accomodation.service;
 
-import codesquad.airbnb.accomodation.dto.PriceAndCount;
 import codesquad.airbnb.accomodation.dto.PriceAndCountStatistics;
-import codesquad.airbnb.accomodation.repository.AccommodationQueryRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+public interface AccommodationStatisticsService {
 
-@Service
-@RequiredArgsConstructor
-public class AccommodationStatisticsService {
-
-    private static final ConcurrentHashMap<LocalDate, PriceAndCountStatistics> simpleCache = new ConcurrentHashMap<>();
-
-    private final AccommodationQueryRepository accommodationQueryRepository;
-    private final AmountUnitPolicy amountUnitPolicy;
-
-    public PriceAndCountStatistics getCachedPriceAndCountStatistics() {
-        LocalDate key = LocalDate.now();
-
-        if (isNotCached(key)) {
-            simpleCache.clear();
-            updateCache(key);
-        }
-
-        return simpleCache.get(key);
-    }
-
-    public AmountUnitPolicy getAmountUnitPolicy() {
-        return amountUnitPolicy;
-    }
-
-    private boolean isNotCached(LocalDate key) {
-        return !simpleCache.containsKey(key);
-    }
-
-    private void updateCache(LocalDate key) {
-        List<PriceAndCount> priceAndCountList = findPriceAndCountStatistics(amountUnitPolicy.getAmount());
-        PriceAndCountStatistics priceAndCountStatistics = new PriceAndCountStatistics(priceAndCountList);
-        simpleCache.put(key, priceAndCountStatistics);
-    }
-
-    private List<PriceAndCount> findPriceAndCountStatistics(long amountUnit) {
-        return accommodationQueryRepository.findPriceAndCountStatisticsByAmountUnit(amountUnit);
-    }
+    PriceAndCountStatistics getCachedPriceAndCountStatistics();
+    AmountUnitPolicy getAmountUnitPolicy();
 }
---
+++ backend/src/main/java/codesquad/airbnb/accomodation/service/DummyAccommodationStatisticsService.java
@@ -0,0 +1,54 @@
+package codesquad.airbnb.accomodation.service;
+
+import codesquad.airbnb.accomodation.dto.PriceAndCount;
+import codesquad.airbnb.accomodation.dto.PriceAndCountStatistics;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DummyAccommodationStatisticsService implements AccommodationStatisticsService {
+
+    private static final AmountUnitPolicy policy = new AmountUnitPolicy();
+
+    @Override
+    public PriceAndCountStatistics getCachedPriceAndCountStatistics() {
+        return new PriceAndCountStatistics(List.of(
+                new PriceAndCount(10000L, 350),
+                new PriceAndCount(20000L, 500),
+                new PriceAndCount(30000L, 750),
+                new PriceAndCount(40000L, 700),
+                new PriceAndCount(50000L, 650),
+                new PriceAndCount(60000L, 800),
+                new PriceAndCount(70000L, 950),
+                new PriceAndCount(80000L, 750),
+                new PriceAndCount(90000L, 800),
+                new PriceAndCount(100000L, 900),
+                new PriceAndCount(110000L, 950),
+                new PriceAndCount(120000L, 900),
+                new PriceAndCount(130000L, 400),
+                new PriceAndCount(140000L, 450),
+                new PriceAndCount(150000L, 350),
+                new PriceAndCount(160000L, 350),
+                new PriceAndCount(170000L, 290),
+                new PriceAndCount(180000L, 400),
+                new PriceAndCount(190000L, 450),
+                new PriceAndCount(200000L, 350),
+                new PriceAndCount(210000L, 350),
+                new PriceAndCount(220000L, 290),
+                new PriceAndCount(230000L, 330),
+                new PriceAndCount(240000L, 350),
+                new PriceAndCount(250000L, 200),
+                new PriceAndCount(260000L, 150),
+                new PriceAndCount(270000L, 120),
+                new PriceAndCount(280000L, 80),
+                new PriceAndCount(290000L, 30),
+                new PriceAndCount(300000L, 15)
+        ));
+    }
+
+    @Override
+    public AmountUnitPolicy getAmountUnitPolicy() {
+        return policy;
+    }
+}
