d217e28735d6a05bd99c9ca6f5658a834d169144
jinan159 <wlsdhks0423@naver.com>
2022-05-30 21:13:07 +0900

    refactor: 숙소 가격 통계 쿼리 리펙토링

--- backend/src/main/java/codesquad/airbnb/accomodation/repository/AccommodationRepositoryCustomImpl.java
+++ backend/src/main/java/codesquad/airbnb/accomodation/repository/AccommodationRepositoryCustomImpl.java
@@ -3,8 +3,8 @@ package codesquad.airbnb.accomodation.repository;
 import codesquad.airbnb.accomodation.domain.QAccommodation;
 import codesquad.airbnb.accomodation.dto.PriceAndCount;
 import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -19,23 +19,19 @@ public class AccommodationRepositoryCustomImpl implements AccommodationRepositor
 
     private final JPAQueryFactory queryFactory;
 
-
     @Override
     public List<PriceAndCount> findPriceAndCountStatisticsByAmountUnit(long amountUnit) {
-        NumberExpression<Long> floorResultOfPriceDivAmountUnit =
-                Expressions.numberTemplate(Long.class, "FLOOR({0} / {1}) * {2}", accommodation.price, amountUnit, amountUnit);
-
-        NumberExpression<Integer> countAll =
-                Expressions.numberTemplate(Integer.class, "COUNT(*)");
+        NumberExpression<Long> priceCutByAmountUnit = accommodation.price.divide(amountUnit).multiply(amountUnit);
+        NumberExpression<Integer> countAll = accommodation.count().intValue();
 
         return queryFactory.select(
-                Projections.fields(PriceAndCount.class,
-                        floorResultOfPriceDivAmountUnit.as("priceRange"),
-                        countAll.as("accommodationCount")
+                Projections.constructor(PriceAndCount.class,
+                        priceCutByAmountUnit,
+                        countAll
                 ))
                 .from(accommodation)
-                .groupBy(floorResultOfPriceDivAmountUnit)
-                .orderBy(floorResultOfPriceDivAmountUnit.asc())
+                .groupBy(priceCutByAmountUnit)
+                .orderBy(priceCutByAmountUnit.asc())
                 .fetch();
     }
 }
