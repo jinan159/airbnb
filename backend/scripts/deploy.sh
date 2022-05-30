9aa4d8c136a98dbb0d59efcefa9b984ef616597a
jinan159 <wlsdhks0423@naver.com>
2022-05-31 03:45:54 +0900

    build: 운영 배포 환경 설정 추가
    - application-prod.properties : 민감정보 암호화 하여 운영서버 설정 추가
    - Dockerfile : prod 프로파일로 실행되도록 변경
    - deploy.sh : docker 실행 로그 남기도록 변경

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+
 sudo chmod +x /home/ec2-user/app/build/libs/*.jar
 
 cd /home/ec2-user/app
 
+LOG_PATH="/home/ec2-user/log"
+
+if [ ! -d $LOG_PATH ]
+then
+  echo `mkdir $LOG_PATH`
+fi
+
+TODAY=`date '+%Y-%m-%dT%H:%M:%S'`
+LOG_NAME="airbnb-$TODAY"
+
+
 sudo docker-compose down
 sudo docker-compose build
-sudo docker-compose up -d
+echo `nohup sudo docker-compose up > $LOG_PATH/$LOG_NAME.log 2> $LOG_PATH/$LOG_NAME.error &`
