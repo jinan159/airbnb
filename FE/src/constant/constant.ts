3fa657602a5badc83fe029bc1fd785234ac8c4de
Ubeom Kim <kimub1204@gmail.com>
2022-06-01 12:45:11 +0900

    feat: footer 구현 완료

--- FE/src/constant/constant.ts
+++ FE/src/constant/constant.ts
@@ -52,3 +52,165 @@ export const CALENDAR_BUTTON_INFOS = [
 ];
 
 export const DAY_TEXTS = ['일', '월', '화', '수', '목', '금', '토'];
+
+export const TRAVEL_INFOS = [
+  {
+    id: 1,
+    src: './assets/images/img.png',
+    alt: '서울',
+    destination: '서울',
+    distance: '차로 30분 거리',
+  },
+  {
+    id: 2,
+    src: './assets/images/img-2.png',
+    alt: '의정부시',
+    destination: '의정부시',
+    distance: '차로 30분 거리',
+  },
+  {
+    id: 3,
+    src: './assets/images/img-4.png',
+    alt: '대구',
+    destination: '대구',
+    distance: '차로 3.5시간 거리',
+  },
+  {
+    id: 4,
+    src: './assets/images/img-6.png',
+    alt: '대전',
+    destination: '대전',
+    distance: '차로 2시간 거리',
+  },
+  {
+    id: 5,
+    src: './assets/images/img-1.png',
+    alt: '광주',
+    destination: '광주',
+    distance: '차로 4시간 거리',
+  },
+  {
+    id: 6,
+    src: './assets/images/img-3.png',
+    alt: '수원시',
+    destination: '수원시',
+    distance: '차로 45분 거리',
+  },
+  {
+    id: 7,
+    src: './assets/images/img-5.png',
+    alt: '울산',
+    destination: '울산',
+    distance: '차로 4.5시간 거리',
+  },
+  {
+    id: 8,
+    src: './assets/images/img-7.png',
+    alt: '부천시',
+    destination: '부천시',
+    distance: '차로 45분 거리',
+  },
+];
+
+export const ANYWHERE_INFOS = [
+  {
+    id: 1,
+    src: './assets/images/Rectangle 2.png',
+    alt: '자연생활을 만끽할 수 있는 숙소',
+    title: '자연생활을 만끽할 수 있는 숙소',
+  },
+  {
+    id: 2,
+    src: './assets/images/Rectangle 2-1.png',
+    alt: '독특한 공간',
+    title: '독특한 공간',
+  },
+  {
+    id: 3,
+    src: './assets/images/Rectangle 2-2.png',
+    alt: '집 전체',
+    title: '집 전체',
+  },
+  {
+    id: 4,
+    src: './assets/images/Rectangle 2-3.png',
+    alt: '반려동물 동반 가능',
+    title: '반려동물 동반 가능',
+  },
+];
+
+export const LNB_INFOS = [
+  {
+    id: 1,
+    title: '소개',
+    link: [
+      '이용 방법',
+      '뉴스룸',
+      '투자자 정보',
+      '호텔투나잇',
+      '비즈니스 프로그램',
+      '호스트 분들이 있기에 가능합니다',
+      '채용정보',
+      '창립자 편지',
+    ],
+  },
+  {
+    id: 2,
+    title: '커뮤니티',
+    link: [
+      '다양성 및 소속감',
+      '접근성',
+      '어소시에이트',
+      '구호 인력을 위한 숙소',
+      '게스트 추천',
+    ],
+  },
+  {
+    id: 3,
+    title: '호스팅하기',
+    link: [
+      '호스팅하기',
+      '숙소 호스팅',
+      '온라인 체험 호스팅하기',
+      '체험 호스팅하기',
+      '책임감 있는 호스팅',
+      '호스트 추천',
+      '자료 센터',
+      '커뮤니티 센터',
+    ],
+  },
+  {
+    id: 4,
+    title: '지원',
+    link: [
+      '코로나19 대응 방안',
+      '도움말 센터',
+      '예약 취소 옵션',
+      '이웃 민원 지원',
+      '신뢰와 안전',
+    ],
+  },
+];
+
+export const FNB_INFOS = [
+  {
+    id: 1,
+    link: '© 2021 Logo, Inc.',
+  },
+  {
+    id: 2,
+    link: '개인정보처리방침',
+  },
+  {
+    id: 3,
+    link: '이용약관',
+  },
+  {
+    id: 4,
+    link: '한국의 변경된 환불 정책',
+  },
+  {
+    id: 5,
+    link: '회사 세부정보',
+  },
+];
--- FE/src/pages/Home/Home.tsx
+++ FE/src/pages/Home/Home.tsx
@@ -1,6 +1,94 @@
 import React from 'react';
-import { Container } from 'pages/Home/Home.styled';
+import {
+  Hero,
+  Footer,
+  Near,
+  NearTitle,
+  NearList,
+  NearItem,
+  TravelImg,
+  TravelDesc,
+  TravelTitle,
+  TravelDistance,
+  AnyWhere,
+  AnyWhereTitle,
+  AnyWhereList,
+  AnyWhereItem,
+  Lodging,
+  LodgingImg,
+  LodgingTitle,
+  Navigator,
+  LNB,
+  LNBList,
+  LNBItem,
+  LNBItemTitle,
+  LNBItemLink,
+  FNB,
+  FNBList,
+  FNBItem,
+  FNBItemLink,
+} from 'pages/Home/Home.styled';
+import { TRAVEL_INFOS, ANYWHERE_INFOS, LNB_INFOS, FNB_INFOS } from 'constant';
+
+import { LNBInterface } from './Home.types';
 
 export function Home(): JSX.Element {
-  return <Container src="/assets/images/hero-img.png" />;
+  const travels = TRAVEL_INFOS.map(info => (
+    <NearItem key={info.id}>
+      <TravelImg src={info.src} alt={info.alt} />
+      <TravelDesc>
+        <TravelTitle>{info.destination}</TravelTitle>
+        <TravelDistance>{info.distance}</TravelDistance>
+      </TravelDesc>
+    </NearItem>
+  ));
+
+  const anywheres = ANYWHERE_INFOS.map(info => (
+    <AnyWhereItem key={info.id}>
+      <Lodging>
+        <LodgingImg src={info.src} alt={info.alt} />
+        <LodgingTitle>{info.title}</LodgingTitle>
+      </Lodging>
+    </AnyWhereItem>
+  ));
+
+  const lnbs = LNB_INFOS.map((info: LNBInterface) => (
+    <LNBList key={info.id}>
+      <LNBItem>
+        <LNBItemTitle>{info.title}</LNBItemTitle>
+      </LNBItem>
+      {info.link.map(text => (
+        <LNBItem key={text}>
+          <LNBItemLink href="#">{text}</LNBItemLink>
+        </LNBItem>
+      ))}
+    </LNBList>
+  ));
+
+  const fnbs = FNB_INFOS.map(info => (
+    <FNBItem key={info.id}>
+      <FNBItemLink href="#">{info.link}</FNBItemLink>
+    </FNBItem>
+  ));
+  return (
+    <>
+      <Hero src="/assets/images/hero-img.png" />
+      <Footer>
+        <Near>
+          <NearTitle>가까운 여행지 둘러보기</NearTitle>
+          <NearList>{travels}</NearList>
+        </Near>
+        <AnyWhere>
+          <AnyWhereTitle>어디서나, 여행은 살아보는 거야!</AnyWhereTitle>
+          <AnyWhereList>{anywheres}</AnyWhereList>
+        </AnyWhere>
+        <Navigator>
+          <LNB>{lnbs}</LNB>
+          <FNB>
+            <FNBList>{fnbs}</FNBList>
+          </FNB>
+        </Navigator>
+      </Footer>
+    </>
+  );
 }
