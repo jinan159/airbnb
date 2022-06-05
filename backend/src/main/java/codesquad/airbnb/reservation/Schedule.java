e993deef4597e99d56ce42398a0232d6ec1853b2
jinan159 <wlsdhks0423@naver.com>
2022-06-06 05:51:21 +0900

    feat: Schedule 에 정적 생성 메소드 추가

--- backend/src/main/java/codesquad/airbnb/reservation/Schedule.java
+++ backend/src/main/java/codesquad/airbnb/reservation/Schedule.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
+import java.time.LocalDateTime;
 
 @Embeddable
 @Getter
@@ -15,5 +16,9 @@ public class Schedule {
 
     @Embedded
     private StartEndDate startEndDate;
-    private int visitors;
+    private Integer visitors;
+
+    public static Schedule of(LocalDateTime startDate, LocalDateTime endDate, Integer visitors) {
+        return new Schedule(new StartEndDate(startDate, endDate), visitors);
+    }
 }
