dd5ded2c105928082b1a63a3f39240243ece1a05
jinan159 <wlsdhks0423@naver.com>
2022-06-05 23:49:39 +0900

    feat: 시작 종료일자 검증로직 추가

--- backend/src/main/java/codesquad/airbnb/reservation/StartEndDate.java
+++ backend/src/main/java/codesquad/airbnb/reservation/StartEndDate.java
@@ -1,18 +1,27 @@
 package codesquad.airbnb.reservation;
 
+import codesquad.airbnb.accomodation.exception.InvalidStartEndDateException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.util.PropertySource;
+import org.springframework.util.comparator.Comparators;
 
 import javax.persistence.Embeddable;
 import java.time.LocalDateTime;
 
 @Embeddable
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor // for @Embeddable
 public class StartEndDate {
 
     private LocalDateTime startDate;
     private LocalDateTime endDate;
+
+    public StartEndDate(LocalDateTime startDate, LocalDateTime endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+
+        if (startDate.isAfter(endDate)) throw new InvalidStartEndDateException();
+    }
 }
