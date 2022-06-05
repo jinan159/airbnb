aa6586a35f429e6eb0cbccbaa55f442d24cc0eff
jinan159 <wlsdhks0423@naver.com>
2022-06-06 04:57:31 +0900

    style: static 변수 대문자로 변경

--- backend/src/test/java/codesquad/airbnb/ApiDocumentUtils.java
+++ backend/src/test/java/codesquad/airbnb/ApiDocumentUtils.java
@@ -3,7 +3,10 @@ package codesquad.airbnb;
 import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
 import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
 
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
 public abstract class ApiDocumentUtils {
     public static final String SCHEME = "http";
