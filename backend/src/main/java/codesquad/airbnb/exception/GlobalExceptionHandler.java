f0902315a5e7b5830c6794773f75fdfb91155c35
jinan159 <wlsdhks0423@naver.com>
2022-06-06 15:32:39 +0900

    refactor: 전역 예외처리 로직 에러 로깅 추가

--- backend/src/main/java/codesquad/airbnb/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/codesquad/airbnb/exception/GlobalExceptionHandler.java
@@ -1,16 +1,20 @@
 package codesquad.airbnb.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = AirbnbRuntimeException.class)
     public ResponseEntity<ErrorResponse> handleSidedishRuntimeException(AirbnbRuntimeException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        log.error(e.getMessage(), e);
 
         return new ResponseEntity<>(errorResponse, e.getStatus());
     }
@@ -18,6 +22,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        log.error(e.getMessage(), e);
 
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
