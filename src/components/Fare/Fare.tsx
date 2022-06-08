f23f5620038d40e8b048b7af083f740a6ef88010
Nick <decay95@naver.com>
2022-06-08 10:32:54 +0900

    요금 정보 기능 구현

--- src/components/Fare/Fare.tsx
+++ src/components/Fare/Fare.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-
-import { InputText } from 'components/InputText';
-
+import React, { useContext, useState } from 'react';
 import { FARE_INFOS } from 'constant';
+import { InputText } from 'components/InputText';
+import { FareContainer } from './Fare.styled';
+import { FareModal } from './FareModal';
+import { FareContext } from '../../contexts/farecontext/farecontext';
 
 export function Fare(): JSX.Element {
-  return <InputText info={FARE_INFOS} />;
+  const { Slidevalue } = useContext(FareContext);
+  const [show, setShow] = useState<boolean>(false);
+  const modalShowHandler = (boolean: boolean): void => {
+    setShow(boolean);
+  };
+  const info = { ...FARE_INFOS, placeHolder: Slidevalue.placeHolder };
+  return (
+    <>
+      <FareContainer onClick={() => modalShowHandler(true)}>
+        <InputText info={info} />
+      </FareContainer>
+      <FareModal show={show} modalShowHandler={modalShowHandler} />
+    </>
+  );
 }
