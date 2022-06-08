4a413fffbef6a40b063324c02c41b703175eff24
Ubeom Kim <kimub1204@gmail.com>
2022-06-08 12:25:21 +0900

    feat: 카카오맵 중심좌표 코드스쿼드로 변경
    추가로 확대, 축소, 좌표 정보 출력 기능 추가

--- FE/src/components/Rooms/Maps/Maps.tsx
+++ FE/src/components/Rooms/Maps/Maps.tsx
@@ -1,29 +1,22 @@
 /* eslint-disable react/react-in-jsx-scope */
 import React from 'react';
 
-import { MapContainer } from './Maps.styled';
+import { MapContainer, CButtonGroup, MapBtn } from './Maps.styled';
 
 import { Map } from '../Map/Map';
 
 export function Maps(): JSX.Element {
-  // useEffect(() => {
-  //   const mapdiv = document.querySelector('#map');
-  //   const options = {
-  //     center: new kakao.maps.LatLng(33.450701, 126.570667),
-  //     level: 3,
-  //   };
-  //   const map = new kakao.maps.Map(mapdiv, options);
-  // }, []);
-
   return (
     <MapContainer>
       <Map />
-      {/* <HeadBtnContainer>
-      <Btn>
-      </Btn>
-      <BtnTitle></BtnTitle>
-    </HeadBtnContain>
-    <Map></Map> */}
+      {/* <CButtonGroup orientation="vertical">
+        <MapBtn variant="contained" href="#contained-buttons" disableRipple>
+          +
+        </MapBtn>
+        <MapBtn variant="contained" href="#contained-buttons" disableRipple>
+          -
+        </MapBtn>
+      </CButtonGroup> */}
     </MapContainer>
   );
 }
