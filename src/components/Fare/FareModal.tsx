a9379a3e656266ec17af6ecc64689e35e883c6c4
Nick <decay95@naver.com>
2022-06-08 10:32:17 +0900

    요금 정보 modal창 구현

---
+++ src/components/Fare/FareModal.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import ModalPortal from 'common/portal';
+import { Backdrop, Modal, Title, PriceRange, Average } from './FareModal.style';
+import { FareProps } from './FareModal.types';
+import FareSlider from './FareSlider';
+import { ChartData } from './Mok';
+import { FareContext } from '../../contexts/farecontext/farecontext';
+import Chart from './FareChart';
+
+export function FareModal({ show, modalShowHandler }: FareProps): JSX.Element {
+  const value = useContext(FareContext);
+  if (show) {
+    return (
+      <ModalPortal>
+        <>
+          <Modal>
+            <Title>가격 범위</Title>
+            <PriceRange>
+              ₩{value.Slidevalue.left} - ₩{value.Slidevalue.right}
+            </PriceRange>
+            <Average>평균 1박 요금은 ₩{ChartData.averagePrice} 입니다.</Average>
+            <Chart />
+            <FareSlider />
+          </Modal>
+          <Backdrop onClick={() => modalShowHandler(false)} />
+        </>
+      </ModalPortal>
+    );
+  }
+
+  return <ModalPortal />;
+}
