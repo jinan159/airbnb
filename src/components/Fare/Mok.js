9c4a33f91062347a75164629c8c9ca9ccc77a129
Nick <decay95@naver.com>
2022-06-08 10:30:33 +0900

    feat: 임시 mok data 추가

---
+++ src/components/Fare/Mok.js
@@ -0,0 +1,45 @@
+export const ChartData = {
+  priceAndCountList: [
+    {
+      priceRange: 10000,
+      accommodationCount: 2,
+    },
+    {
+      priceRange: 20000,
+      accommodationCount: 4,
+    },
+    {
+      priceRange: 30000,
+      accommodationCount: 6,
+    },
+    {
+      priceRange: 40000,
+      accommodationCount: 15,
+    },
+    {
+      priceRange: 50000,
+      accommodationCount: 12,
+    },
+    {
+      priceRange: 60000,
+      accommodationCount: 9,
+    },
+    {
+      priceRange: 70000,
+      accommodationCount: 7,
+    },
+    {
+      priceRange: 80000,
+      accommodationCount: 6,
+    },
+    {
+      priceRange: 90000,
+      accommodationCount: 4,
+    },
+    {
+      priceRange: 100000,
+      accommodationCount: 7,
+    },
+  ],
+  averagePrice: 24000,
+};
